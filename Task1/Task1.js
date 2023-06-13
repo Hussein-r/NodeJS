d46330a7735ef2762c0d499c19bc20f4bc94ebf4
hussein-r <hussein.rk94@gmail.com>
2023-06-13 15:39:19 +0300

    refactoring

--- Task1/Task1.js
+++ Task1/Task1.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const spreadArrOfArr = function (arr) {
   let res = [];
   for (i of arr) {
@@ -6,48 +6,68 @@ const spreadArrOfArr = function (arr) {
   }
   return res;
 };
-//////////1
-let obj = JSON.parse(fs.readFileSync("problem1.json", { encoding: "utf-8" }));
-console.log(obj);
+const listCatFriendsActivities = function (data) {
+  const activities = data.catFriends.map((friend) => friend.activities);
+  console.log(activities);
+  return data;
+};
+const printCatFriendsNames = function (data) {
+  const names = data.catFriends.map((friend) => friend.name);
+  console.log(names);
+  return data;
+};
+const printCatFriendsTotalWeight = function (data) {
+  let sum = 0;
+  data.catFriends.map((friend) => {
+    sum += friend.weight;
+  });
+  console.log(`the total sum of cat friends weight is ${sum}`);
+  return data;
+};
 
-/////////2
-obj.height = 50;
-obj.weight = 25;
-
-/////////3
-obj.name = "Fluffyy";
-
-//////////4
-console.log(obj.catFriends.map((friend) => friend.activities));
-
-////////5
-console.log(obj.catFriends.map((friend) => friend.name));
-
-///////6
-let sum = 0;
-obj.catFriends.map((friend) => {
-  sum += friend.weight;
-});
-console.log(sum);
-
-///////////7
-console.log([
-  ...obj.activities,
-  ...spreadArrOfArr(obj.catFriends.map((friend) => friend.activities)),
-]);
-
-/////////8
-obj.catFriends.map((friend) => {
-  if (["bar", "foo"].includes(friend.name)) {
-    friend.activities.push("activity1", "activity2");
-  }
-});
-console.log(obj.catFriends);
-
-///////////9
-obj.catFriends.map((friend) => {
-  if (friend.name === "bar") {
-    friend.furcolor = "black";
-  }
-});
-console.log(obj.catFriends);
+const totalActivitiesOfAllCats = function (data) {
+  const activities = [
+    ...data.activities,
+    ...spreadArrOfArr(data.catFriends.map((friend) => friend.activities)),
+  ];
+  console.log(activities);
+  return data;
+};
+const addTwoMoreActivities = function (data) {
+  data.catFriends.map((friend) => {
+    if (["bar", "foo"].includes(friend.name)) {
+      friend.activities.push("activity1", "activity2");
+    }
+  });
+  console.log(data.catFriends);
+  return data;
+};
+const addHeightWeight = function (data) {
+  data.height = 50;
+  data.weight = 25;
+  return data;
+};
+const updateName = function (data) {
+  data.name = "Fluffyy";
+  return data;
+};
+const updateColor = function (data) {
+  data.catFriends.map((friend) => {
+    if (friend.name === "bar") {
+      friend.furcolor = "black";
+    }
+  });
+  console.log(data.catFriends);
+  return data;
+};
+fs.readFile("problem1.json", { encoding: "utf-8" })
+  .then(JSON.parse)
+  .then(addHeightWeight)
+  .then(updateName)
+  .then(listCatFriendsActivities)
+  .then(printCatFriendsNames)
+  .then(printCatFriendsTotalWeight)
+  .then(totalActivitiesOfAllCats)
+  .then(addTwoMoreActivities)
+  .then(updateColor)
+  .then(console.log);
